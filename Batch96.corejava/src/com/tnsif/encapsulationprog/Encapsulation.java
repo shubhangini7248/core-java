39969d8228c8bb72df77a84d30150e87d84cd01a
Shubhangi <Shubhangi@LAPTOP-NHH3R2JR>
2024-06-25 18:57:47 +0530

    first commit

---
+++ Batch96.corejava/src/com/tnsif/encapsulationprog/Encapsulation.java
@@ -0,0 +1,13 @@
+//program to demonstrate encapsulation
+package com.tnsif.encapsulationprog;
+
+public class Encapsulation {
+    int serialNum;//Datavariable
+    String name;
+    int age;
+    
+    void show()//Member function
+    {
+    	System.out.println ("serialnumber"+serialNum+"\t Name"+"\t Age:"+ age);
+    }
+}
---
+++ Batch96.corejava/src/com/tnsif/encapsulationprog/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.encapsulationprog;
+
+public class EncapsulationDemo {
+ 
+	public static void main(String[] args) {
+		//TODO Auto-generated method stub
+		
+		Encapsulation ob=new Encapsulation();
+		ob.name="suma";
+		ob.age=33;
+		ob.serialNum=22;
+		ob.show();
+	}
+}
