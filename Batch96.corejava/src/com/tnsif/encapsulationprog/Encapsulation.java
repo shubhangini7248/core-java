92a11a8b80c62cda2e11bd5488eb76c8810c771f
Shubhangi <Shubhangi@LAPTOP-NHH3R2JR>
2024-06-26 09:05:04 +0530

    second commit

--- Batch96.corejava/src/com/tnsif/encapsulationprog/Encapsulation.java
+++ Batch96.corejava/src/com/tnsif/encapsulationprog/Encapsulation.java
@@ -5,9 +5,34 @@ public class Encapsulation {
     int serialNum;//Datavariable
     String name;
     int age;
-    
-    void show()//Member function
-    {
-    	System.out.println ("serialnumber"+serialNum+"\t Name"+"\t Age:"+ age);
-    }
+     
+// to display string representation of the object
+	@Override
+	public String toString() {
+		return "Encapsulation [serialNum=" + serialNum + ", name=" + name + ", age=" + age + "]";
+	}
+
+	public int getSerialNum() {
+		return serialNum;
+	}
+
+	public void setSerialNum(int serialNum) {
+		this.serialNum = serialNum;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
 }
--- Batch96.corejava/src/com/tnsif/encapsulationprog/EncapsulationDemo.java
+++ Batch96.corejava/src/com/tnsif/encapsulationprog/EncapsulationDemo.java
@@ -6,9 +6,15 @@ public class EncapsulationDemo {
 		//TODO Auto-generated method stub
 		
 		Encapsulation ob=new Encapsulation();
-		ob.name="suma";
-		ob.age=33;
-		ob.serialNum=22;
-		ob.show();
+//	ob.name="Suma";// calling the variables
+//	    ob.age=33;
+//	    ob.serialNum=22;
+		
+	    ob.setAge(22);
+	    ob.setName("shubhangi");
+	    ob.setSerialNum(55);
+	    System.out.println(ob);
+	    
 	}
+	
 }
